b92cde460c799625c8f5fc0d0a29b22c5c48385d
joelamaya19 <joelamaya99@gmail.com>
2024-06-05 17:32:31 -0500

    created typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,46 @@
 const typeDefs = `
+type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Book {
+    _id: ID
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+}
+
+input BookInput {
+    author: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+}
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(book: BookInput!): User
+    deleteBook(bookId: String!): User
+}
 
 `;
 
